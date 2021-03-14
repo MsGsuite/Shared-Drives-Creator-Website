e2f346e04eebf8e636bf94635d8336e7bf027338
MsGsuite <80416370+MsGsuite@users.noreply.github.com>
2021-03-14 18:34:50 +0100

    Update SharedDriveCreator.js

--- SharedDriveCreator.js
+++ SharedDriveCreator.js
@@ -2,7 +2,7 @@ var authConfig = {
     version: "2.0",
     dailyLimit: true, // Whether to limit each mailbox to submit requests only once a day
     client_id: "", // Google Client ID
-    client_secret: "", // Google Client Secret. The refresh token goes to line 598
+    client_secret: "", // Google Client Secret. The refresh token goes to line 597
     domain: "! ", //College name to display
     black_list: [],
 };
